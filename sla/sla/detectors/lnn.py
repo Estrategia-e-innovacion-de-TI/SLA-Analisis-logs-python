0a1bc53d2f0d94afeb8f6368c744df56bc27b6c9
Nicolas-dds <n.laverdem@uniandes.edu.co>
2025-02-27 19:06:25 -0500

    Add lnn

---
+++ sla/sla/detectors/lnn.py
@@ -0,0 +1,13 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+from sklearn.preprocessing import StandardScaler
+
+from tensorflow import keras
+from ncps import wirings
+from ncps.keras import LTC
