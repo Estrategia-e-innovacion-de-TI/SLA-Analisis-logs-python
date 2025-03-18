29b74fa6107eb30f772223b1bc9ba3c4cb763d74
JoseMVergara <josevergara.udm>
2025-03-18 15:40:20 -0500

    Add static, dynamic and explainer plots

---
+++ sla/sla/visualization/__init__.py
@@ -0,0 +1,10 @@
+from .visualization import AnomalyVisualizer, SHAPVisualizer
+
+"""Module of detectors.
+
+A detector detects anomalous time points from time series.
+
+"""
+
+
+__all__ = ['AnomalyVisualizer', 'SHAPVisualizer']
